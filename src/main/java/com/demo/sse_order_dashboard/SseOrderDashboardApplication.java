3a5c514ec3ee1cd14e90cc17fa38a74b2b089117
Ariful Haque Noman <haqueariful33@gmail.com>
2025-11-25 10:36:38 +0600

    Initial commit - project setup

---
+++ src/main/java/com/demo/sse_order_dashboard/SseOrderDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.demo.sse_order_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SseOrderDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SseOrderDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/sse_order_dashboard/SseOrderDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.sse_order_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SseOrderDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
